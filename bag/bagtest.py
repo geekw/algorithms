1b61fde1caf250513f283da6e8ac090d4c76455e
Robin Wei <robinwei1979@gmail.com>
2017-08-04 18:11:49 +0800

    Implemented and tested Bag using Python List

---
+++ bag/bagtest.py
@@ -0,0 +1,11 @@
+from listbag import Bag
+
+mybag = Bag()
+mybag.add("Australia")
+mybag.add("Brazil")
+mybag.add("China")
+mybag.add("Denmark")
+mybag.add("England")
+
+for country in mybag:
+    print country
