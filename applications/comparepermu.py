242d961de4c6624b1b0b9b7425e0f870174db5b5
Robin Wei <robinwei1979@gmail.com>
2017-08-16 17:35:01 +0800

    Compared the performance of my permutation implementation with the one provided by the standard library. The result is astonishing. For a list of 10 items, mine uses approximately 29 seconds, yet the standard one uses less than 1 msgit add .git add .git add .

---
+++ applications/comparepermu.py
@@ -0,0 +1,13 @@
+from itertools import permutations
+from applications.myutils.combinatorial import Permutation
+
+def permustd(theList):
+    return permutations(theList)
+
+def permumine(theList):
+    perms = Permutation(theList)
+    return perms.solutions()
+
+mylist = [i for i in range(10)]
+perms1 = permustd(mylist)
+perms2 = permumine(mylist)
